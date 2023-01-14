e0179901b6dc4cdcd81e7507dba533cee67e3974
alanmitic <alanmitic@outlook.com>
2023-01-14 14:09:05 +0000

    Add console input support

--- gocalc.go
+++ gocalc.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"alanmitic/gocalc/expreval"
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Go Calc")
-	evaluator := new(expreval.Evaluator)
+	evaluator := expreval.NewEvaluator()
+	reader := bufio.NewReader(os.Stdin)
 
-	result, err := evaluator.Evaluate("1 + 3 * 4")
-	fmt.Println("result = ", result)
-	fmt.Println("err = ", err)
+	fmt.Println("gocalc version 0.0.1\n\nType 'exit' and ENTER to quit.")
+
+	for {
+		fmt.Print("gocalc >> ")
+		exprOrCmd, _ := reader.ReadString('\n')
+		if strings.TrimSpace(exprOrCmd) == "exit" {
+			return
+		}
+
+		result, err := evaluator.Evaluate(exprOrCmd)
+		if err != nil {
+			fmt.Println("ERROR:", err)
+		} else {
+			fmt.Println(result)
+		}
+	}
 }
